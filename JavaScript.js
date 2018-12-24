68a629918a987c99ff13c10ba671388f4001ad53
Chaturvedi <shivang.chaturvedi@accenture.com>
2018-12-25 00:19:21 +0530

    ES6 Homework #1

--- JavaScript.js
+++ JavaScript.js
@@ -7,4 +7,23 @@ console.log("10");
 var a = "210191";
 a = +a;
 console.log(a);
-alert(a);
+
+//Boolean functions
+
+var b  = true;
+
+console.log(Boolean(a));
+
+var myObject = {
+    1: "shivang",
+    sayHi: function(){
+    console.log("Hey Man!")
+    }
+};
+
+myObject[2] = 'Chaturvedi';
+myObject[6] = 'Ashton';
+
+console.log(myObject);
+
+myObject.sayHi();
---
+++ song.js
@@ -0,0 +1,19 @@
+var oSongName = "Patience";
+var oGenre = "Acoustic rock";
+var oSinger = "Guns N' Roses";
+var oYearOfRelease = "April 1989";
+var oRecorded = "1988";
+var oProducer = "Guns N' Roses, " + "Mike Clink & " + " ‎John Shanks" ;
+var oReleaseFormat = "Vinyl, cassette";
+var oLabel = "Geffen";
+var oPlayTime = "5:56";
+
+console.log(oSongName);
+console.log(oGenre);
+console.log(oSinger);
+console.log(oYearOfRelease);
+console.log(oRecorded);
+console.log(oProducer);
+console.log(oReleaseFormat);
+console.log(oLabel);
+console.log(oPlayTime);
